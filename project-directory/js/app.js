fa922c5ec314fdeb0a31e8ac7c93b7b24babe891
‘Nathan <ncash13@hotmail.com>
2018-06-05 16:16:01 -0500

    Simplified JS and added console.logs

--- project-directory/js/app.js
+++ project-directory/js/app.js
@@ -1,46 +1,66 @@
 'use strict'
 
-var answer1 = prompt('Did Nathan Cashman graduate from college?');
-if (answer1.toUpperCase().trim() === 'YES' || answer1.toUpperCase().trim() === 'Y'){
+var question = 'Did Nathan Cashman graduate from college?';
+var answer = prompt(question);
+if (answer.toUpperCase().trim() === 'YES' || answer.toUpperCase().trim() === 'Y'){
     alert('That is correct!');
-} else if (answer1.toUpperCase().trim() === 'NO' || answer1.toUpperCase().trim() === 'N'){
+    console.log("Question 1: '" +question+"' was answered correctly!");
+} else if (answer.toUpperCase().trim() === 'NO' || answer.toUpperCase().trim() === 'N'){
     alert('Actually, I graduated from Drake University.');
+    console.log("Question 1: '" +question+"' was answered incorrectly!");
 } else {
     alert("C'mon, at least try to do this.");
+    console.log("Question 1: '" +question+"' was not answered!");
 }
 
-var answer2 = prompt('Does Nathan Cashman play bass?');
-if (answer2.toUpperCase().trim() === 'YES' || answer2.toUpperCase().trim() === 'Y'){
+question = 'Does Nathan Cashman play bass?';
+answer = prompt(question);
+if (answer.toUpperCase().trim() === 'YES' || answer.toUpperCase().trim() === 'Y'){
     alert("I wish! No, I'm terrible at string instruments.");
-} else if (answer2.toUpperCase().trim() === 'NO' || answer2.toUpperCase().trim() === 'N'){
+    console.log("Question 2: '" +question+"' was answered incorrectly!");
+} else if (answer.toUpperCase().trim() === 'NO' || answer.toUpperCase().trim() === 'N'){
     alert("You're right, string instruments are some of the only instruments I can't play well.");
+    console.log("Question 2: '" +question+"' was answered correctly!");
 } else {
     alert('Dude, just humor me.');
+    console.log("Question 2: '" +question+"' was not answered!");
 }
 
-var answer3 = prompt('Does Nathan Cashman live in Cedar Rapids?');
-if (answer3.toUpperCase().trim() === 'YES' || answer3.toUpperCase().trim() === 'Y'){
+question = 'Does Nathan Cashman live in Cedar Rapids?';
+answer = prompt(question);
+if (answer.toUpperCase().trim() === 'YES' || answer.toUpperCase().trim() === 'Y'){
     alert("Yep, I've lived here on and off for about 11 years.");
-} else if (answer3.toUpperCase().trim() === 'NO' || answer3.toUpperCase().trim() === 'N'){
+    console.log("Question 3: '" +question+"' was answered correctly!");
+} else if (answer.toUpperCase().trim() === 'NO' || answer.toUpperCase().trim() === 'N'){
     alert("Actually I've lived here for quite some time.");
+    console.log("Question 3: '" +question+"' was answered incorrectly!");
 } else {
     alert('Why are you even here?');
+    console.log("Question 3: '" +question+"' was not answered!");
 }
 
-var answer4 = prompt('Does Nathan Cashman like anime?');
-if (answer4.toUpperCase().trim() === 'YES' || answer4.toUpperCase().trim() === 'Y'){
+question = 'Does Nathan Cashman like anime?';
+answer = prompt(question);
+if (answer.toUpperCase().trim() === 'YES' || answer.toUpperCase().trim() === 'Y'){
     alert('Mmhmm, I even keep up with the current seasons in Japan.');
-} else if (answer4.toUpperCase().trim() === 'NO' || answer4.toUpperCase().trim() === 'N'){
+    console.log("Question 4: '" +question+"' was answered correctly!");
+} else if (answer.toUpperCase().trim() === 'NO' || answer.toUpperCase().trim() === 'N'){
     alert('Actually, I was a the advisor to my anime club in college.');
+    console.log("Question 4: '" +question+"' was answered incorrectly!");
 } else {
     alert("Don't you have better things to do?");
+    console.log("Question 4: '" +question+"' was not answered!");
 }
 
-var answer5 = prompt('Does Nathan Cashman live with his parents?');
-if (answer5.toUpperCase().trim() === 'YES' || answer5.toUpperCase().trim() === 'Y'){
+question = 'Does Nathan Cashman live with his parents?';
+answer = prompt(question);
+if (answer.toUpperCase().trim() === 'YES' || answer.toUpperCase().trim() === 'Y'){
     alert("Don't rub it in...");
-} else if (answer5.toUpperCase().trim() === 'NO' || answer5.toUpperCase().trim() === 'N'){
+    console.log("Question 5: '" +question+"' was answered correctly!");
+} else if (answer.toUpperCase().trim() === 'NO' || answer.toUpperCase().trim() === 'N'){
     alert("CORRECT! They live with me!");
+    console.log("Question 5: '" +question+"' was answered correctly!");
 } else{
     alert("Y'know what? I'm done with you... Child.");
+    console.log('Error: you have been abandoned.');
 }
