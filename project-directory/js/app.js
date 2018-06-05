6f618f9075074ffa899db6649892b95ff424c7a0
‘Nathan <ncash13@hotmail.com>
2018-06-05 14:14:08 -0500

    About me info added

--- project-directory/js/app.js
+++ project-directory/js/app.js
@@ -1,1 +1,10 @@
 'use strict'
+
+var answer1 = prompt('Did Nathan Cashman graduate from college?')
+if (answer1.toUpperCase().trim() === 'YES' || answer1.toUpperCase().trim() === 'Y'){
+    alert('That is correct!');
+} else if (answer1.toUpperCase().trim() === 'NO' || answer1.toUpperCase().trim() === 'N'){
+    alert('Actually, I graduated from Drake University.');
+} else {
+    alert("C'mon, at least try to do this.");
+}
